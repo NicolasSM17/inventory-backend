7741e551f3dbfdabc7fc565132cbe378849bb257
Danko539 <nicolasxartier@gmail.com>
2023-10-15 16:41:55 -0500

    proyecto base de control de inventario

---
+++ src/main/java/pe/nico/cibertec/ProyectoInventarioApplication.java
@@ -0,0 +1,13 @@
+package pe.nico.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/nico/cibertec/ProyectoInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.nico.cibertec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
