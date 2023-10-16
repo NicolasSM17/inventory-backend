ef64b76203a7f630bf21a88519626428494e7754
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-10-15 23:33:46 -0500

    servicio guardar categoria

--- src/main/java/pe/nico/cibertec/service/ICategoryService.java
+++ src/main/java/pe/nico/cibertec/service/ICategoryService.java
@@ -2,9 +2,11 @@ package pe.nico.cibertec.service;
 
 import org.springframework.http.ResponseEntity;
 
+import pe.nico.cibertec.model.Category;
 import pe.nico.cibertec.response.CategoryResponseRest;
 
 public interface ICategoryService {
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 }
